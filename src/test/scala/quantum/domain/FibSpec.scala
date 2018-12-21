08f048d137f783d15f45d1a740e65735537e86c8
logicalguess <logicalguess@gmail.com>
2018-12-21 14:37:07 -0700

    fibonacci - ry

--- src/test/scala/quantum/domain/FibSpec.scala
+++ src/test/scala/quantum/domain/FibSpec.scala
@@ -1,7 +1,7 @@
 package quantum.domain
 
 import org.scalatest.FlatSpec
-import quantum.domain.Gate.{H, ZERO, controlledRy, controlled, wire}
+import quantum.domain.Gate.{H, ZERO, Ry, controlledRy, controlled, wire}
 import quantum.domain.QState.pure
 
 class FibSpec extends FlatSpec {
@@ -23,19 +23,19 @@ class FibSpec extends FlatSpec {
     }
   }
 
-  "fib" should "work with controlled H" in {
+  "fib" should "work with controlled Ry" in {
 
     def fib(n: Int): QState = {
       var state = pure(Word.fromInt(0, n))
-      for (i <- 0 until n) state = state >>= wire(i, H)
-      for (i <- 0 until n - 1)  state = state >>= controlledRy(i, i + 1, -math.Pi/2) // >>= wire(i + 1, Z)
+      for (i <- 0 until n) state = state >>= wire(i, Ry(math.Pi/2))
+      for (i <- 0 until n - 1)  state = state >>= controlledRy(i, i + 1, -math.Pi/2)
 
       state
     }
 
     for (n <- 1 to 10) {
       val q = fib(n)
-      val states = q.bins.filter({ case (w, a) => a.norm2 > 0.000001 })
+      val states = q.bins.filter({ case (w, a) => a.norm2 > 0.0000000000000000000000000000001 })
       println(s"F($n) = ${states.size}")
     }
   }
